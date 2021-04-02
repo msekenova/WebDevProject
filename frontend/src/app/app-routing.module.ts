57c7700257b91a296052ba9c2aec7010c28b9964
msekenova <sekenova.meyirim@gmail.com>
2021-04-02 23:58:14 +0600

    Added new components and paths.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {CategoryComponent} from './category/category.component';
+import {AuthorizationComponent} from './authorization/authorization.component';
+import {ShoppingCartComponent} from './shopping-cart/shopping-cart.component';
+import {NotfoundComponent} from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'category', component: CategoryComponent},
+  {path: 'authorization', component: AuthorizationComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CategoryComponent } from './category/category.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ShoppingCartComponent,
+    CategoryComponent,
+    AuthorizationComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
