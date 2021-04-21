9b7e4af9fbe8bf398651e8ebd5c7d7e5bcc95483
msekenova <sekenova.meyirim@gmail.com>
2021-04-21 23:37:31 +0600

    registration, login forms and navigation bar are done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { LoginComponent } from './login/login.component';
     CategoryComponent,
     AuthorizationComponent,
     NotfoundComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
